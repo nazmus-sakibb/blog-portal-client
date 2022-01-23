e2bbfc4d352460661e98394ce26b59f4e03da0d9
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-01-23 18:29:41 +0600

    client site initiated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import CTA from './components/cta/CTA';
+import Feature from './components/feature/Feature';
+import Navbar from './components/navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <h1>Blog Portal</h1>
+            <Article/>
+            <Brand/>
+            <CTA/>
+            <Feature/>
+            <Navbar/>
+        </div>
+    );
+};
 
 export default App;
