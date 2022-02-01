f67a0a11dd1a2fb79cbd96e172aaedf8ab85d891
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-02-01 22:59:01 +0600

    header input done

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
     return (
@@ -10,6 +12,15 @@ const Header = () => {
                 <div className="gpt3__header-content__input">
                     <input type="email" placeholder="Your Email Address" />
                     <button type="button">Get Started</button>
+                </div>
+
+                <div className="gpt3__header-content__people">
+                    <img src={people} alt="" />
+                    <p>1,600 people requested access a visit in last 24 hours</p>
+                </div>
+
+                <div className="gpt3__header-image">
+                    <img src={ai} alt="AI" />
                 </div>
             </div>
         </div>
